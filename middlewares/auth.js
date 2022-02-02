e861d3698bf748e8372a0cc746c64c76579a3117
Tatyana D <yana.dok.89@mail.ru>
2022-02-02 12:48:05 +0300

    auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,28 +3,18 @@ const UnauthorizedError = require('../errors/unauthorizedErr');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 const auth = (req, res, next) => {
-  if (!req.cookies.jwt) {
-    throw new UnauthorizedError(
-      'Токен не передан или передан не в том формате'
-    );
-  }
-
   const token = req.cookies.jwt;
-
   let payload;
-
   try {
     payload = jwt.verify(
       token,
       NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'
     );
   } catch (err) {
-    throw new UnauthorizedError('Передан некорректный токен');
+    throw new UnauthorizedError('Необходима авторизация');
   }
-
   req.user = payload;
-
-  return next();
+  next();
 };
 
 module.exports = auth;

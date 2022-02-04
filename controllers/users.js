6db20638a1e6e853dc183d3f17ee8711e1cec803
Tatyana D <yana.dok.89@mail.ru>
2022-02-04 17:24:55 +0300

    jwt

--- controllers/users.js
+++ controllers/users.js
@@ -77,20 +77,19 @@ function login(req, res, next) {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign(
-        { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-        { expiresIn: '7d' }
-      );
-      res
-        .cookie('jwt', token, {
-          maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
-          sameSite: true,
-        })
-        .send({ message: 'Успешная авторизация' });
+      res.send({
+        token: jwt.sign(
+          { _id: user._id },
+          NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+          {
+            expiresIn: '7d',
+          }
+        ),
+      });
     })
-    .catch(next);
+    .catch(() => {
+      next(new BadRequestError('Неправильные почта или пароль'));
+    });
 }
 
 function logout(req, res) {
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,18 +4,24 @@ const UnauthorizedError = require('../errors/unauthorizedErr');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
+
+  const token = authorization.replace('Bearer ', '');
   let payload;
+
   try {
     payload = jwt.verify(
       token,
-      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'
+      NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key'
     );
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
+
   req.user = payload;
   next();
 };
-
 module.exports = auth;
